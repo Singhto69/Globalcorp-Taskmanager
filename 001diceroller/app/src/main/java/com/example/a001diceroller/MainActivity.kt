797bf1f9f192aaec160a5cf42cce81a4f5260ea7
Singhto69 <Timmerskold@hotmail.com>
2023-04-10 15:15:08 +0200

    diceroller
    added images

--- 001diceroller/app/src/main/java/com/example/a001diceroller/MainActivity.kt
+++ 001diceroller/app/src/main/java/com/example/a001diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a001diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,23 +12,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val luckyNumber = 4
         val rollButton: Button = findViewById(R.id.button0)
-        val resultTextView: TextView = findViewById(R.id.textView0)
-        Dice(420)
+        val dice0 = Dice(6)
 
         rollButton.setOnClickListener {
             //val toast = Toast.makeText(this, "Dice Rolled!" , Toast.LENGTH_SHORT).show()
-            rollDice()
+            rollDice(luckyNumber,dice0)
         }
 
     }
 
-    private fun rollDice() {
-        val dice0 = Dice(6)
+    private fun rollDice(luckyNumber: Int , dice : Dice) {
+
+
         val resultTextView: TextView = findViewById(R.id.textView0)
 
-        val diceRoll = dice0.roll()
+        val diceImage: ImageView = findViewById(R.id.dice)
+
+        val diceRoll = dice.roll()
+
         resultTextView.text = diceRoll.toString()
+
+        /*if (diceRoll == luckyNumber){
+            val toast = Toast.makeText(this, "Number hit" , Toast.LENGTH_SHORT).show()
+        }
+        else {
+            val toast = Toast.makeText(this, "Number not hit , you suck" , Toast.LENGTH_SHORT).show()
+        }*/
+        val drawableResource = when (diceRoll) {
+            //luckyNumber -> Toast.makeText(this, "Number hit" , Toast.LENGTH_SHORT).show()
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            luckyNumber -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {
+                R.drawable.dice_6
+            }
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
 
     }
 }
