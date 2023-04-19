5f95b762d88e895ff3aebf948f642be1bade0178
Singhto69 <Timmerskold@hotmail.com>
2023-04-19 22:26:01 +0200

    nav training

--- 004android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ 004android-basics-kotlin-words-app-starter/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
 import androidx.annotation.RequiresApi
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -53,7 +54,7 @@ class LetterAdapter :
             .from(parent.context)
             .inflate(R.layout.item_view, parent, false)
         // Setup custom accessibility delegate to set the text read
-        layout.accessibilityDelegate = Accessibility
+        layout.accessibilityDelegate = WordAdapter
         return LetterViewHolder(layout)
     }
 
@@ -64,10 +65,16 @@ class LetterAdapter :
         val item = list.get(position)
         holder.button.text = item.toString()
         holder.button.setOnClickListener {
+            /*
+            --- Send via intent --
             val context = holder.itemView.context
             val intent = Intent(context, DetailActivity::class.java)
             intent.putExtra(DetailActivity.LETTER, holder.button.text.toString())
-            context.startActivity(intent)
+            context.startActivity(intent)*/
+            val action = LetterListFragmentDirections
+                .actionLetterListFragmentToWordListFragment(letter = "b")
+            holder.view.findNavController().navigate(action)
+            // holder.button.text.toString()
         }
     }
 
